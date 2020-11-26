f51b5289c980fa5b9ecaebe18cef67580b32f964
gilad <giladamar1212@gmail.com>
2020-11-26 13:24:25 +0200

    start

---
+++ myBank.h
@@ -0,0 +1,9 @@
+extern double accounts[50][2] ;
+void openAccount(double amount);
+void balance(int acc_num);
+void deposit(int acc_num ,double amount);
+void withdraw(int acc_num ,double amount);
+void closeAccount(int acc_num);
+void addDependPercent(double percent);
+void printall();
+void closeAllAccounts();
